9de7abca08aeb19e53b280e0c097840f515cbc62
Nicol Umanzor <umanzorn@uci.edu>
2026-06-05 14:38:08 -0700

    rest pass

--- ff-app/src/Pages/ResetPassword.jsx
+++ ff-app/src/Pages/ResetPassword.jsx
@@ -20,7 +20,7 @@ export function ResetPassword() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:3001/auth/reset-password", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/auth/reset-password`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ token, password }),
